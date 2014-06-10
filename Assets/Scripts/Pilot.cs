85e5dbca312c883ec6bf14ffb7f0e4bbcea922d0
Kael Spencer <kaelspencer@gmail.com>
2014-06-09 20:29:17 -0700

    Make Flying Better
    It feels like the wrong thing, but instead of adding torque to rotate
    the copter when the right stick is moved, a simple rotation is used.
    This gives a better flying feel.

--- Assets/Scripts/Pilot.cs
+++ Assets/Scripts/Pilot.cs
@@ -4,55 +4,16 @@ using System;
 
 public class Pilot : MonoBehaviour
 {
-    // Use this for initialization
-    void Start()
+    void Update()
     {
-        Debug.Log("Starting up.");
+        float x = -90.0f * Input.GetAxis("RotationX");
+        float z = 90.0f * Input.GetAxis("RotationZ");
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(x, 0, z), Time.time * 0.1f);
     }
 
     void FixedUpdate()
     {
-        rigidbody.AddForce(Physics.gravity * -1);
-        bool rotationalInput = false;
-
-        rigidbody.AddForce(transform.up * 15 * Input.GetAxis("Vertical"));
-        rigidbody.AddRelativeTorque(new Vector3(1, 0, 0) * -0.05f * Input.GetAxis("RotationX"));
-        rigidbody.AddRelativeTorque(new Vector3(0, 0, 1) * 0.05f * Input.GetAxis("RotationZ"));
-
-        if (Math.Abs(Input.GetAxis("RotationX")) > 0.01f || Math.Abs(Input.GetAxis("RotationZ")) > 0.01f)
-        {
-            rotationalInput = true;
-        }
-
-        // Counteract rotations.
-        if (!rotationalInput && (Math.Abs(transform.up.x) > 0.01f || Math.Abs(transform.up.y) < 0.99f || Math.Abs(transform.up.z) > 0.01f))
-        {
-            Vector3 torque = new Vector3();
-
-            // Rotate about X, XY plane.
-            if (transform.up.z > 0 && rigidbody.angularVelocity.x > 0)
-            {
-                torque.x = -100 * (1 - transform.up.y);
-            }
-            else if (transform.up.z < 0 && rigidbody.angularVelocity.x < 0)
-            {
-                torque.x = 100 * (1 - transform.up.y);
-            }
-
-            // Rotate about Z, YZ plane.
-            if (transform.up.x > 0 && rigidbody.angularVelocity.z < 0)
-            {
-                torque.z = 100 * (1 - transform.up.y);
-            }
-            else if (transform.up.x < 0 && rigidbody.angularVelocity.z > 0)
-            {
-                torque.z = -100 * (1 - transform.up.y);
-            }
-
-            if (torque.magnitude > 0)
-            {
-                rigidbody.AddTorque(torque);
-            }
-        }
+        //rigidbody.AddForce(Physics.gravity * -1);
+        rigidbody.AddForce(transform.up * 20 * Input.GetAxis("Vertical"));
     }
 }
